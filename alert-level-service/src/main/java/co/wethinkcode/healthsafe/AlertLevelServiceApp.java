6fbbe4336d68ab2e37034cc71e34fd7433f86d7e
Twanano Rikhotso <ntwananorikhotso05@gmail.com>
2026-09-16 13:47:14 +0200

    Implement alert level retrieval endpoint

--- alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
@@ -4,12 +4,20 @@ import io.javalin.Javalin;
 
 public class AlertLevelServiceApp {
 
+    private static int currentAlertLevel = 0;
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7032);
 
         app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/alert-level", ctx -> {
+            ctx.json(new AlertLevelResponse(currentAlertLevel));
+        });
 
         // TODO (Tracks the hospital Emergency Status (0-8, 8 = full Code Blue).)
         // Add domain endpoints for alert-level-service here.
+    }
+
+    public record AlertLevelResponse(int level) {
     }
 }
